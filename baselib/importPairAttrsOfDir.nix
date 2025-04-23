72fd775a063919110a3d3455028a1216cb96390d
nrs-status <sebaarsim@gmail.com>
2025-04-22 23:52:06 -0300

    fixing mkMyPkgs and adding withDebug to it

--- baselib/importPairAttrsOfDir.nix
+++ baselib/importPairAttrsOfDir.nix
@@ -1,11 +1,15 @@
 { pkgslib }:
-{ filePath, inputForImportPairs }:
-let
+{ filePath, inputForImportPairs, activateDebug ? false }:
+let total = rec {
   filesList = (import ./listDirIgnoring.nix { inherit pkgslib; }) {
     ignore = [];
     dir = filePath;
   };
   functionToMap = path: import ./mkImportPair.nix { inherit pkgslib; } { importInputs = inputForImportPairs; filePath = path; };
   importPairList = builtins.map functionToMap filesList;
-in
-builtins.listToAttrs importPairList
+  final = builtins.listToAttrs importPairList
+
+}; in (import ./withDebug.nix) activateDebug {
+  debug = total;
+  nondebug = total.final;
+}
