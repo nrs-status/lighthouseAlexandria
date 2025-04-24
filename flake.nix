6fb479532f7fa4b248f0cc786eecdb1e587e791b
nrs-status <sebaarsim@gmail.com>
2025-04-24 11:53:20 -0300

    factoring out nixpkgs input so that it is instead an argument, adding typechecklib as output in this flake instead

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
     nixvimFlake.url = "github:nix-community/nixvim";
   };
 
   outputs = inputs:
-  #eventually find a way to refactor so as to have this system spec in the final flake
-    let pkgs = import inputs.nixpkgs { system = "x86_64-linux"; }; in
-    {
-      pkgslib = pkgs.lib;
-      baselib = import ./baselib { 
+    rec {
+      baselib = { pkgs }: import ./baselib { 
         pkgslib = pkgs.lib; 
         nixvimFlake = inputs.nixvimFlake;
       };
-      inherit pkgs;
+      tclib = { pkgs, typesSource }: import ./tclib/ {
+        libs = {
+          pkgslib = pkgs.lib;
+          inherit baselib;
+        };
+        inherit typesSource;
+      };
     };
 }
